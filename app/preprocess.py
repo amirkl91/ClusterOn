4ba24b4c77d4ba7d97df3f6815c6d8ca08ea918f
Amir Kleiner <amir.kleiner@weizmann.ac.il>
2024-09-12 15:22:43 +0300

    Check for and add (if applicable) height to buildings dataframe

--- app/preprocess.py
+++ app/preprocess.py
@@ -5,16 +5,28 @@ import pandas
 def get_buildings(buildings, local_crs=None):
     ngeoms = len(buildings.geom_type.value_counts())
     if ngeoms > 1:
-        if sum(buildings.building_type == 'Polygon'):
+        if sum(buildings.geom_type == 'Polygon') > 0:
             polygontype = 'Polygon'
-        elif sum(buildings.build_type == 'MultiPolygon'):
+        elif sum(buildings.geom_type == 'MultiPolygon') > 0:
             polygontype = 'MultiPolygon'
         else:
             raise ValueError('No polygons or multi-polygons in building data')
-        buildings = buildings[buildings.geom_type=='Polygon'].reset_index(drop=True)[['geometry']]
+        buildings = buildings[buildings.geom_type==polygontype].reset_index(drop=True)
         if local_crs:
             buildings.to_crs(local_crs)
-    return buildings
+    
+    # This is not perfect. It assumes that in case there's more than one height value, the building height is the 1st.
+    if 'height' not in buildings.keys():
+        if len(height_vars := buildings.keys()[['height' in key for key in buildings.keys()]]) == 0:
+            print('No building heights in data')
+            return buildings[['geometry']]
+        elif len(height_vars) == 1:
+            buildings['height'] = buildings[height_vars]
+        else:
+            print('here')
+            buildings['height'] = buildings[height_vars[0]]
+        
+    return buildings[['geometry', 'height']]
 
 if __name__=='__main__':
     import os
@@ -23,9 +35,11 @@ if __name__=='__main__':
     if not (pardir := str(Path(__file__).absolute().parent)) in path:
         path.append(pardir)
     
-    datadir = '../../../Michaels_data/All_layers_2'
+    datadir = '../../Michaels_data/All_layers_2'
     gdb_file = os.path.join(datadir, os.listdir(datadir)[76])
     print(gdb_file)
     gdf = gpd.read_file(gdb_file)
-
+    buildings = get_buildings(gdf)
+    print(gdf.head())
+    print(buildings.head())
 
