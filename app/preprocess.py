46b1fa5eede464d7ed1c06355b9e13b8c213e2f2
Amir Kleiner <amir.kleiner@weizmann.ac.il>
2024-09-15 14:39:18 +0300

    Add tessellation function

--- app/preprocess.py
+++ app/preprocess.py
@@ -1,6 +1,8 @@
 import geopandas as gpd
 import momepy
 import pandas
+import numpy as np
+from time import time
 
 def get_buildings(buildings, local_crs=None, height_name=None, min_area=20):
     '''
@@ -18,7 +20,7 @@ def get_buildings(buildings, local_crs=None, height_name=None, min_area=20):
     Removes duplicated buildings, by geometry.
     Does not (yet) remove overlapping but not duplicated buildings!
     '''
-
+    buildings = buildings.to_crs(local_crs) if local_crs is not None else buildings
     categories = ['geometry']
     if height_name:
         buildings['height'] = buildings[height_name]
@@ -41,7 +43,7 @@ def get_buildings(buildings, local_crs=None, height_name=None, min_area=20):
     buildings = buildings[buildings.area > min_area]
 
 
-    return buildings.to_crs(local_crs) if local_crs else buildings
+    return buildings.reset_index(drop=True)
 
 def get_streets(streets, local_crs=None):
     '''
@@ -57,6 +59,8 @@ def get_streets(streets, local_crs=None):
     Removes duplicated streets, if any present.
     Does not, yet, consolidate multiple crossings at intersection into single node.
     '''
+
+    streets.to_crs(local_crs) if local_crs else streets
     streets = momepy.remove_false_nodes(streets)
     streets.geometry = momepy.close_gaps(streets, tolerance=0.25)
     streets = momepy.roundabout_simplification(streets)
@@ -68,14 +72,66 @@ def get_streets(streets, local_crs=None):
     streets['length'] = streets.length
     streets = streets[['geometry', 'length']]
 
-    return streets.to_crs(local_crs) if local_crs else streets
+    return streets
 
-def tessellation(buildings, streets=None, mode='enclosed'):
+def tessellation(buildings, streets=None, tess_mode='enclosed', clim='adaptive'):
     '''
     TBD
     Creates tessellations from buildings dataframe, if "enclosed" mode then uses streets as enclosures.
     '''
-    pass
+    try:
+        limit = momepy.buffered_limit(buildings, clim)
+    except:
+        if clim=='adaptive':
+            print('Failed creating limit in adaptive mode. Resetting limit to 100')
+            clim = 100
+            limit = momepy.buffered_limit(buildings, clim)
+    if tess_mode=='enclosed':
+        if streets is None:
+            raise ValueError('No streets provided for enclosed tessellation')
+        t0 = time()
+        enclosures = momepy.enclosures(streets, limit)
+        enc_time = time()
+        try:
+            print('Trying enclosed tessellations')
+            tessellations = momepy.enclosed_tessellation(buildings, enclosures)
+        except:
+            try:
+                print('Failed producing enclosed tessellations')
+                print('Trying morphometric tessellations')
+                tessellations, limit = tessellation(buildings, tess_mode='morphometric')
+            except:
+                if clim=='adaptive':
+                    print('Faild with "adaptive" limits. Resetting limit to 100 and retrying morphological tessellation')
+                    tessellations, limit = tessellation(buildings, tess_mode='morphometric', clim=100)
+
+        tess_time = time()
+    
+        print(f'Computed enclosures: {enc_time-t0} sec\nComputed tessellations: {tess_time-enc_time} sec')
+
+        return tessellations, enclosures, limit
+    elif tess_mode=='morphometric':
+        t0 = time()
+        tessellations = momepy.morphological_tessellation(buildings, clip=limit)
+        print(f'Computed tessellations: {time()-t0} s')
+        return tessellations, limit
+    
+    else:
+        raise NotImplementedError('Requested tessellation mode not implemented')
+        
+def find_overlaps(gdf):
+    # Buffer
+    buffer = gdf.copy()
+    buffer.geometry = buffer.geometry.buffer(3)
+
+    # Overlaps check
+    intersecting_gdf = buffer.sjoin(buffer, predicate="intersects")
+    intersecting_gdf = intersecting_gdf.loc[
+        intersecting_gdf.index != intersecting_gdf.index_right
+    ]
+    overlaps_ids = np.unique(intersecting_gdf.index)  # list overlapping IDs
+    return(overlaps_ids)
+
 
 if __name__=='__main__':
     import os
@@ -91,9 +147,13 @@ if __name__=='__main__':
 
     # datadir = '../../Michaels_data/All_layers_2'
     # gdb_file = os.path.join(datadir, os.listdir(datadir)[76])
-    # # print(gdb_file)
-    # gdf = gpd.read_file(gdb_file)
-    # buildings = get_buildings(gdf)
+    datadir = '/Users/amirkl/PhD/DSSG/michaels_data/All_layers'
+    gdb_file = Path(datadir, os.listdir(datadir)[-1], 'commondata', 'jps_reka.gdb')
+    # print(gdb_file)
+    gdf = gpd.read_file(gdb_file)
+    ### Add check of geometry type to OSM datasets?
+    # gdf = osmnx.features_from_place(place, tags={'building':True})
+    buildings = get_buildings(gdf, local_crs='EPSG:2039')
     # print(gdf.head())
     # print(buildings.head())
 
@@ -106,10 +166,13 @@ if __name__=='__main__':
         node_geometry=False,
         fill_edge_geometry=True,
     ).reset_index(drop=True)
-    print(len(osm_streets))
+    # print(len(osm_streets))
     # print(osm_streets.head())
 
     streets = get_streets(osm_streets)
-    print(len(streets))
+    # print(len(streets))
     # print(streets.head())
+    overlaps = find_overlaps(buildings)
+    tessellations, enclosures, limits = tessellation(buildings, streets, tess_mode='enclosed' )
+    # tessellations, limits = tessellation(buildings, tess_mode='morphometric' )
 
