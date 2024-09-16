3a5edc5700fcc5342f304597711c2add8795a867
Amir Kleiner <amir.kleiner@weizmann.ac.il>
2024-09-16 13:43:28 +0300

    Add main script to module

---
+++ app/__main__.py
@@ -0,0 +1,8 @@
+'''
+File to be run automatically when the module is run as a script from the command line.
+Usage: python app
+Should probably use argparser to parse default arguments etc.
+'''
+
+print('Thanks for running the city clustering and explanation software.')
+print('UNDER CONSTRUCTION')
