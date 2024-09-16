0a6f46df3d7f6ff0405c1feded9ba71fa3f33b64
Amir Kleiner <amir.kleiner@weizmann.ac.il>
2024-09-16 10:22:48 +0300

    Refactor building metrics into smaller chunks

--- app/calculate_building_metrics.py
+++ app/calculate_building_metrics.py
@@ -19,16 +19,13 @@ from libpysal import graph
 
 import helper_functions
 
-local_crs = "EPSG:2039"
-
-
-
-def get_all_building_metrics(buildings: gpd.geodataframe, height_column_name=None):
+def building_metrics(buildings: gpd.geodataframe, height_column_name=None):
     buildings['area'] = buildings.area
     if height_column_name is not None:
         buildings['floor_area'] = momepy.floor_area(buildings['area'], buildings[height_column_name])
         buildings['volume'] = momepy.volume(buildings['area'], buildings[height_column_name])
         buildings['form_factor'] = momepy.form_factor(buildings, buildings[height_column_name])
+
     # Basic geometric properties
     buildings['perimeter'] = buildings.geometry.length
     buildings['shape_index'] = momepy.shape_index(buildings, momepy.longest_axis_length(buildings))
@@ -38,25 +35,24 @@ def get_all_building_metrics(buildings: gpd.geodataframe, height_column_name=Non
     buildings['convexity'] = momepy.convexity(buildings)
     buildings['courtyard_area'] = momepy.courtyard_area(buildings)
     buildings['courtyard_index'] = momepy.courtyard_index(buildings)
-    # buildings['corners'] = momepy.corners(buildings, include_interiors=True)   # include_interiors=False works only for polygons not multipolygons
+    buildings['corners'] = momepy.corners(buildings, include_interiors=True)   # include_interiors=False works only for polygons not multipolygons
     buildings['fractal_dimension'] = momepy.fractal_dimension(buildings)
     buildings['facade_ratio'] = momepy.facade_ratio(buildings)
-    # buildings_centroid_corner_distance = momepy.centroid_corner_distance(buildings)
-    # buildings['centroid_corner_distance_mean'] = buildings_centroid_corner_distance['mean']
-    # buildings['centroid_corner_distance_std'] = buildings_centroid_corner_distance['std']
+    buildings[['centr_corn_dist_mean', 'centr_corn_dist_std']] = momepy.centroid_corner_distance(buildings)
+
     # More complex morphological metrics
     buildings['orientation'] = momepy.orientation(buildings)
     buildings['longest_axis_length'] = momepy.longest_axis_length(buildings)
     buildings['equivalent_rectangular_index'] = momepy.equivalent_rectangular_index(buildings)
     buildings['elongation'] = momepy.elongation(buildings)
-    # buildings['linearity'] = momepy.linearity(buildings) # does not work on buildings just on streets
     buildings['rectangularity'] = momepy.rectangularity(buildings)
-    # buildings['squareness'] = momepy.squareness(buildings, include_interiors=True)   # without interiors works only for polygons not multipolygons
+    buildings['squareness'] = momepy.squareness(buildings, include_interiors=True)   # without interiors works only for polygons not multipolygons
     buildings['shared_walls_length'] = momepy.shared_walls(buildings) / buildings['perimeter']
     buildings['perimeter_wall'] = momepy.perimeter_wall(buildings)  # TODO: check for possible parameters
     buildings['perimeter_wall'] = buildings['perimeter_wall'].fillna(0)
+    
+def building_graph_metrics(buildings):
     # Metrics related to building adjacency and Graph
-
 
     delaunay = graph.Graph.build_triangulation(buildings.centroid, coplanar='clique').assign_self_weight()
     orientation = momepy.orientation(buildings)
@@ -68,27 +64,38 @@ def get_all_building_metrics(buildings: gpd.geodataframe, height_column_name=Non
     buildings['mean_interbuilding_distance'] = momepy.mean_interbuilding_distance(buildings, delaunay,
                                                                                     knn15)  # TODO: check for queen1 and queen3
     buildings['neighbour_distance'] = momepy.neighbor_distance(buildings, delaunay)
-    buildings['courtyards_num'] = momepy.courtyards(buildings,
-                                                    contiguity)  # Calculate the number of courtyards within the joined structure
+    buildings['courtyards_num'] = momepy.courtyards(buildings, contiguity)  # Calculate the number of courtyards within the joined structure
+    return contiguity
 
+def building_tess_metrics(buildings, tessellations, contiguity):
+    ### Has wierd errors. Dissregard for now
     # metrics related to tessellation
-    limit = helper_functions.buffered_limit(buildings, buffer='adaptive')
-    tessellation = momepy.morphological_tessellation(buildings, clip=limit)  # need adjustment
-    blg_orient = momepy.orientation(buildings)
-    tess_orient = momepy.orientation(tessellation)
-    buildings['cell_orientation'] = momepy.cell_alignment(blg_orient, tess_orient)
-    buildings['num_of_neighbours'] = momepy.neighbors(tessellation, contiguity, weighted=True)
-
+    tess_orient = momepy.orientation(tessellations)
+    buildings['cell_orientation'] = momepy.cell_alignment(buildings['orientation'], tess_orient)
+    buildings['num_of_neighbours'] = momepy.neighbors(tessellations, contiguity, weighted=True)
 
 if __name__=='__main__':
-    from data_input import load_buildings_from_osm
-    from preprocess import get_buildings
+    from time import time
+    import preprocess as pp
+    from data_input import load_buildings_from_osm, load_roads_from_osm
 
     place = 'Jerusalem'
     local_crs = 'EPSG:2039'
     network_type = 'drive'
 
-    buildings = load_buildings_from_osm(place)
-    buildings = get_buildings(buildings=buildings, local_crs=local_crs, )
+    streets = load_roads_from_osm(place, network_type=network_type)
+    streets, intersections = pp.get_streets(streets=streets, local_crs=local_crs, get_nodes=True)
 
-    # get_all_building_metrics(buildings, height_column_name='height')
+    buildings = load_buildings_from_osm(place)
+    buildings = pp.get_buildings(buildings=buildings, streets=streets, intersections=intersections, local_crs=local_crs, )
+
+    tesselations, enclosures = pp.get_tessellation(buildings=buildings, streets=streets, 
+                                            tess_mode='enclosed', clim='adaptive')
+
+    t0 = time()
+    building_metrics(buildings)
+    print(f'Building only metrics: {(t1:=time())-t0} s')    
+    contiguity = building_graph_metrics(buildings)
+    print(f'Building graph metrics: {(t2:=time()) - t1} s')
+    # building_tess_metrics(buildings, tesselations, contiguity)
+    # print(f'Building tesselation metrics: {time()-t2} s')
