f52c706b50378c0fed4ca50db7571c65ed5b7eb1
Amir Kleiner <amir.kleiner@weizmann.ac.il>
2024-09-17 16:20:52 +0300

    Add tessellation metrics and fix naming of other metrics

--- app/metrics.py
+++ app/metrics.py
@@ -24,7 +24,7 @@ def generate_building_metrics(buildings: gpd.geodataframe, height_column_name=No
     buildings['circular_compactness'] = momepy.circular_compactness(buildings)
     buildings['square_compactness'] = momepy.square_compactness(buildings)
     buildings['weighted_axis_compactness'] = momepy.compactness_weighted_axis(buildings)
-    buildings['convexity'] = momepy.convexity(buildings)
+    # buildings['convexity'] = momepy.convexity(buildings)
     buildings['courtyard_area'] = momepy.courtyard_area(buildings)
     buildings['courtyard_index'] = momepy.courtyard_index(buildings)
     buildings['fractal_dimension'] = momepy.fractal_dimension(buildings)
@@ -41,9 +41,7 @@ def generate_building_metrics(buildings: gpd.geodataframe, height_column_name=No
     buildings['perimeter_wall'] = buildings['perimeter_wall'].fillna(0)
     # Metrics related to building adjacency and Graph
 
-    return buildings
-
-def generate_graph_metrics(buildings, streets, tessellation, coplanar='raise',knnA=15, knnB=5):
+def generate_graph_building_metrics(buildings, streets, queen_1):
     '''
     :param buildings:
     :param streets:
@@ -51,70 +49,66 @@ def generate_graph_metrics(buildings, streets, tessellation, coplanar='raise',kn
     :param coplanar: switch to clique if there's overlapping polygons
     :return:
     '''
-    delaunay = graph.Graph.build_triangulation(buildings.centroid, coplanar='clique').assign_self_weight()
-    blg_orient = momepy.orientation(buildings)
-    buildings['alignment'] = momepy.alignment(blg_orient, delaunay)
+    queen_3 = queen_1.higher_order(k=3)
+    build_q1 = graph.Graph.build_contiguity(buildings)
 
-    knn_1 = graph.Graph.build_knn(buildings.centroid, k=knnA, coplanar='clique')  # adjust k if needed
-    contiguity = graph.Graph.build_contiguity(buildings)
-    buildings['adjacency'] = momepy.building_adjacency(contiguity, knn_1)  # TODO: check for queen1 and queen3
-    buildings['mean_interbuilding_distance'] = momepy.mean_interbuilding_distance(buildings, delaunay, knn_1)  # TODO: check for queen1 and queen3
-    buildings['neighbour_distance'] = momepy.neighbor_distance(buildings, delaunay)
-    buildings['courtyards_num'] = momepy.courtyards(buildings,
-                                                    contiguity)  # Calculate the number of courtyards within the joined structure
+    buildings['alignment'] = momepy.alignment(buildings['orientation'], queen_1)
+    buildings['adjacency'] = momepy.building_adjacency(build_q1, queen_3) 
+    buildings['mean_interbuilding_distance'] = momepy.mean_interbuilding_distance(buildings, queen_1, queen_3)
+    buildings['neighbour_distance'] = momepy.neighbor_distance(buildings, queen_1)
+    buildings['courtyards_num'] = momepy.courtyards(buildings, build_q1)  # Calculate the number of courtyards within the joined structure
 
     if 'height' in buildings.keys():
-        print('with height')
         prof_metrics = ['width', 'openness', 'width_dev', 'height', 'height_dev', 'hw_ratio']
         heights = buildings['height']
     else:
-        print('no height')
         prof_metrics = ['width', 'openness', 'width_dev']
         heights = None
     streets[prof_metrics] = momepy.street_profile(streets, buildings, height=heights)
 
-    blg_orient = momepy.orientation(buildings)
-    str_orient = momepy.orientation(streets)
     buildings["street_index"] = momepy.get_nearest_street(buildings, streets)
-    buildings['street_alignment'] = momepy.street_alignment(blg_orient, str_orient, buildings["street_index"])
+    buildings['street_alignment'] = momepy.street_alignment(buildings['orientation'], streets['str_orientation'], buildings["street_index"])
+    return queen_3
 
-    return buildings, streets
+def generate_tessellation_metrics(tessellations, buildings):
+    queen_1 = graph.Graph.build_contiguity(tessellations, rook=False)
+    tessellations['tess_area'] = tessellations.area
+    tessellations['tess_circ_compactness'] = momepy.circular_compactness(tessellations)
+    tessellations['tess_convexity'] = momepy.convexity(tessellations)
+    tessellations['tess_neighbors'] = momepy.neighbors(tessellations, queen_1, weighted=True)
+    tessellations['tess_covered_area'] = queen_1.describe(tessellations.area)['sum']
+    tessellations['tess_build_area_raio'] = buildings.area / tessellations.area
+    tessellations['tess_orientation'] = momepy.orientation(tessellations)
+    tessellations['tess_building_orientation'] = (tessellations['tess_orientation']-buildings['orientation']).abs()
+    tessellations['tess_alignment'] = momepy.alignment(tessellations['tess_orientation'], queen_1)
+    tessellations['tess_equivalent_rect_index'] = momepy.equivalent_rectangular_index(tessellations)
+    return queen_1
 
-
-def generate_all_metrics(buildings, streets, tessellation, height_column_name=None):
-    buildings = generate_building_metrics(buildings, height_column_name)
-    # streets = generate_streets_metrics(streets)
-    buildings, streets = generate_graph_metrics(buildings, streets, tessellation)
-    return buildings, streets
 
 def generate_streets_metrics(streets_gdf):
-    streets_geometry = streets_gdf["geometry"]
 
-    streets_gdf["orientation"] = momepy.orientation(streets_geometry)
-    streets_gdf["longest_axis_length"] = momepy.longest_axis_length(streets_geometry)
-    streets_gdf["compactness_weighted_axis"] = momepy.compactness_weighted_axis(
-        streets_geometry
-    )
-    streets_gdf["linearity"] = momepy.linearity(streets_geometry)
-    streets_gdf["length"] = streets_geometry.length
+    streets_gdf["str_orientation"] = momepy.orientation(streets_gdf)
+    streets_gdf["str_longest_axis"] = momepy.longest_axis_length(streets_gdf)
+    # streets_gdf["compactness_weighted_axis"] = momepy.compactness_weighted_axis(streets_gdf)
+    streets_gdf["str_linearity"] = momepy.linearity(streets_gdf)
+    streets_gdf["str_length"] = streets_gdf.length
     # Calculates natural continuity and hierarchy of street networks
-    coins = momepy.COINS(streets_gdf)
-    stroke_attr = coins.stroke_attribute()
-    # print(stroke_attr.head())
-    streets_gdf["stroke_id"] = stroke_attr
-    # Group by stroke_id to calculate stroke-level continuity (total length of each stroke)
-    stroke_continuity = streets_gdf.groupby("stroke_id")["length"].sum().reset_index()
-    stroke_continuity.columns = ["stroke_id", "continuity"]
-    # Merge continuity back into the streets GeoDataFrame
-    streets_gdf = streets_gdf.merge(stroke_continuity, on="stroke_id", how="left")
-    # Rank strokes by length to create a hierarchy
-    stroke_continuity["hierarchy"] = stroke_continuity["continuity"].rank(
-        ascending=False
-    )
-    # Merge hierarchy back into the streets GeoDataFrame
-    streets_gdf = streets_gdf.merge(
-        stroke_continuity[["stroke_id", "hierarchy"]], on="stroke_id", how="left"
-    )
+    # coins = momepy.COINS(streets_gdf)
+    # stroke_attr = coins.stroke_attribute()
+    # streets_gdf["stroke_id"] = stroke_attr
+    # # Group by stroke_id to calculate stroke-level continuity (total length of each stroke)
+    # stroke_continuity = streets_gdf.groupby("stroke_id")["length"].sum().reset_index()
+    # stroke_continuity.columns = ["stroke_id", "continuity"]
+    # # Merge continuity back into the streets GeoDataFrame
+    # streets_gdf = streets_gdf.merge(stroke_continuity, on="stroke_id", how="left")
+    # # Rank strokes by length to create a hierarchy
+    # stroke_continuity["hierarchy"] = stroke_continuity["continuity"].rank(
+    #     ascending=False
+    # )
+    # # Merge hierarchy back into the streets GeoDataFrame
+    # streets_gdf = streets_gdf.merge(
+    #     stroke_continuity[["stroke_id", "hierarchy"]], on="stroke_id", how="left"
+    # )
 
 def generate_junctions_metrics(streets, verbose=False):
 
@@ -126,25 +120,18 @@ def generate_junctions_metrics(streets, verbose=False):
 
     # Calculates clustering coefficient for each node (how connected are its neighbors)
     graph = momepy.clustering(graph)
-
     # Calculates cyclomatic complexity (number of loops) around each node
     graph = momepy.cyclomatic(graph, verbose=verbose)
-
     # Calculate the ratio of edges to nodes in a subgraph around each node
     graph = momepy.edge_node_ratio(graph, verbose=verbose)
-
     # Calculate the gamma index (connectivity) for each node
     graph = momepy.gamma(graph, verbose=verbose)
-
     # Calculate the mean node degree for a subgraph around each node
     graph = momepy.mean_node_degree(graph, verbose=verbose)
-
     # Calculate the mean distance to neighboring nodes for each node
     graph = momepy.mean_node_dist(graph, verbose=verbose)
-
     # Calculate the meshedness for subgraph around each node
     graph = momepy.meshedness(graph, verbose=verbose)
-
     # Calculate the density of nodes around each node
     graph = momepy.node_density(graph, radius=5, verbose=verbose)
 
