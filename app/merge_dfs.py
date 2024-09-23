56b35dc2397fd0f1d40c271ba94a99c3bd590422
DavidOriel <davoriel@gmail.com>
2024-09-23 14:40:42 +0300

    finished additing stats

--- app/merge_dfs.py
+++ app/merge_dfs.py
@@ -1,30 +1,43 @@
 import momepy
 import libpysal
 import pandas as pd
+from clustergram import Clustergram
+
 
 def merge_all_metrics(tessellations, buildings, streets, junctions):
     merged = buildings.merge(
-        tessellations.drop(columns=['geometry', ]), 
-        left_index=True, right_on='tID', how='left'
-        )
-    merged = merged.merge(
-    streets.drop(columns=['geometry','node_start', 'node_end']),
-    left_on='street_index', right_index=True, how='left'
+        tessellations.drop(
+            columns=[
+                "geometry",
+            ]
+        ),
+        left_index=True,
+        right_on="tID",
+        how="left",
     )
     merged = merged.merge(
-        junctions.drop(columns=['x','y','geometry','nodeID','cluster']),
-        left_on='junction_index', right_index=True, how='left'
+        streets.drop(columns=["geometry", "node_start", "node_end"]),
+        left_on="street_index",
+        right_index=True,
+        how="left",
+    )
+    merged = merged.merge(
+        junctions.drop(columns=["x", "y", "geometry", "nodeID", "cluster"]),
+        left_on="junction_index",
+        right_index=True,
+        how="left",
     )
     return merged
+
 
 def compute_percentiles(merged, queen_3):
     percentiles = []
     for column in merged.columns.drop(
         [
-            'geometry',
-            'street_index',
-            'junction_index',
-            'tID',
+            "geometry",
+            "street_index",
+            "junction_index",
+            "tID",
         ]
     ):
         print(column)
@@ -33,11 +46,31 @@ def compute_percentiles(merged, queen_3):
         percentiles.append(perc)
 
     percentiles_joined = pd.concat(percentiles, axis=1)
-    metrics_df = pd.concat([merged.drop(
-        columns=['geometry', 'street_index', 'junction_index', 'tID',]), 
-        percentiles_joined], axis=1)
+    metrics_df = pd.concat(
+        [
+            merged.drop(
+                columns=[
+                    "geometry",
+                    "street_index",
+                    "junction_index",
+                    "tID",
+                ]
+            ),
+            percentiles_joined,
+        ],
+        axis=1,
+    )
     return metrics_df
+
 
 def standardize_df(df):
     standardized = (df - df.mean()) / df.std()
     return standardized
+
+
+def cluster_gdf(gdf):
+    only_metrics = gdf.drop(columns=["geometry"])
+    cgram = Clustergram(range(1, 12), n_init=10, random_state=42)
+    cgram.fit(only_metrics.fillna(0))
+    gdf["cluster"] = cgram.labels[10].values
+    return gdf
